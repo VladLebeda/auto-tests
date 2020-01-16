d78cfa3cf50d4c3fc0e102b96c9ba7c7221acfd6
VladLebeda <vladlebeda@gmail.com>
2020-01-16 21:17:05 +0300

    Updating script for 3_6_3

--- Part_3/test_decoding_aliens[3_6_3].py
+++ Part_3/test_decoding_aliens[3_6_3].py
@@ -1,51 +1,39 @@
 import time
 import math
-import selenium
-
-answer = math.log(int(time.time()))
-
+import pytest
+from selenium import webdriver
 
 
 
-#open browser
+#fixture, initializing browser and cleaning up after the tests?
 
-
-
-#input answer
-
-
-#send answer
-
-#wait for feedback
-
-
-#check if the text == "Correct!"
-
-
-https://stepik.org/lesson/236895/step/1
-https://stepik.org/lesson/236896/step/1
-https://stepik.org/lesson/236897/step/1
-https://stepik.org/lesson/236898/step/1
-https://stepik.org/lesson/236899/step/1
-https://stepik.org/lesson/236903/step/1
-https://stepik.org/lesson/236904/step/1
-https://stepik.org/lesson/236905/step/1
-
-
-
-
-
-try:
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
     browser = webdriver.Chrome()
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+    
+@pytest.mark.parametrize('lesson_link', [236895, 236896, 236897, 236898, 236899, 236903, 236904, 236905])
+def test_parametrize_lesson_link(browser, 'lesson_link'):
+    link = f"https://stepik.org/lesson/{lesson_link}/step/1"
+
+    #open browser
     browser.get(link)
     answer_field = browser.find_element_by_css_selector('[class="textarea string-quiz__textarea ember-text-area ember-view"]')
+
     #calculating current answer and sending it to the answer field
     answer = math.log(int(time.time()))
     answer_field.send_keys(answer)
+
     #finding send button
     answer_button = browser.find_element_by_css_selector('[class="submit-submission"]')
     answer_button.click()
 
-#textarea
-    find_by_css_selector('[class="smart-hints__hint"]')
-    
+    #waiting for loading
+    time.sleep(1)
+
+    #check if the text == "Correct!"
+    success_field = browser.find_by_css_selector('[class="smart-hints__hint"]').text
+    assert success_field == "Correct!", "Incorrect answer!"
