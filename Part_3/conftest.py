45a387c7c1346c16bee5967b05831b99a29b1e04
VladLebeda <vladlebeda@gmail.com>
2020-01-18 20:37:26 +0300

    Updating conftest.py with language option for the parser.

--- Part_3/conftest.py
+++ Part_3/conftest.py
@@ -5,23 +5,31 @@ from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default=None,
+    parser.addoption('--browser_name', action='store', default='chrome',
                      help="Choose browser: chrome or firefox")
-    
-@pytest.fixture(scope="function")
+    parser.addoption('--language', action='store', default='en',
+                     help='Current page language')
+
+
+@pytest.fixture(scope='function')
 def browser(request):
-    browser_name = request.config.getoption("browser_name")
+    browser_name = request.config.getoption('browser_name')
+    browser_language = request.config.getoption('language')
     browser = None
-    if browser_name == "chrome":
-        print("\nstart chrome browser for test..")
+
+    if browser_name == 'chrome':
+        print('\nstart chrome browser for test..')
         browser = webdriver.Chrome()
     elif browser_name == "firefox":
-        print("\nstart firefox browser for test..")
+        print('\nstart firefox browser for test..')
         browser = webdriver.Firefox()
     else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
+        raise pytest.UsageError('--browser_name should be chrome or firefox')
     yield browser
-    print("\nquit browser..")
+    print('\nquit browser..')
     browser.quit()
+
